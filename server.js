d7a77d0796f0c18abd68f1b3625d38f72d30ea10
ANDRIANARY-Andrianjaka-Stan <njakaSandrian@gmail.com>
2026-01-16 11:21:47 +0300

    Changer l'affichage

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('manahoana anja');
+  res.send('Bonjour à tous');
 });
 
 app.listen(PORT, HOST, () => {
