685da9108eb119fe35ba5bf8fb14af47c425017d
ANDRIANARY-Andrianjaka-Stan <njakaSandrian@gmail.com>
2026-05-17 17:31:43 +0300

    test CI/CD project

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Je vous remercie pour votre attention et votre présence.');
+  res.send('test.');
 });
 
 app.listen(PORT, HOST, () => {
